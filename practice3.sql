8e798eaa9feee14db668f3fc372414cb31b70f6e
nofacer <nofacer@outlook.com>
2019-05-08 21:56:04 +0800

    pratice3

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,5 @@
-#取得平均薪水最高的公司
-#输出公司名称和平均薪水：companyName avgSalary
-
+-- 取得平均薪水最高的公司
+-- 输出公司名称和平均薪水:companyName avgSalary
+select companyName,avgSalary from 
+(select company,avg(salary) as avgSalary from Employee group by company) as t left join Company c on t.company=c.id;
 
